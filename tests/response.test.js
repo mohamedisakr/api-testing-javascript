145447eb05ccc123cde8bff04509f6bf0045b4c9
mohamedisakr <mohamedisakr@gmail.com>
2020-11-27 03:12:55 +0200

    implementing api response

---
+++ tests/response.test.js
@@ -0,0 +1,32 @@
+const app = require("../src/app");
+const request = require("supertest");
+const expect = require("chai").expect;
+
+describe("response", () => {
+  //
+  it("json response", () => {
+    request(app)
+      .get("/course")
+      .end((err, res) => {
+        expect(res.body.id).to.be.equal("1");
+      });
+  });
+
+  //
+  it("test response", () => {
+    request(app)
+      .get("/course")
+      .end((err, res) => {
+        expect(res.text).to.contain("1");
+      });
+  });
+
+  //
+  it("status response", () => {
+    request(app)
+      .get("/course")
+      .end((err, res) => {
+        expect(res.ok).to.be.true;
+      });
+  });
+});
